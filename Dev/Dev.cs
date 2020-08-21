c4739bda18c06cb547f12a2a43791daea0a3a7c7
Augusto <augustomp55@gmail.com>
2020-08-21 01:06:07 +0000

    Added Dev.cs to /Dev

---
+++ Dev/Dev.cs
@@ -0,0 +1,17 @@
+﻿namespace Scouts.Backend.Dev
+{
+    public static class Constants
+    {
+        public static string[] SubscriptionTags { get; set; } = { "default" };
+        public static string NewsNotificationHubName { get; set; } = "scouts-news";
+        public static string NewsFullAccessConnectionString { get; set; } = "Endpoint=sb://scouts.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=3FhKkS9zfUTmo+Ch5WK8hsznfvxj7bVFnsgTXpFUS+A=";
+    }
+
+    public class DeviceInstallation
+    {
+        public string InstallationId { get; set; }
+        public string Platform { get; set; }
+        public string PushChannel { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
