0b10073d35eec3a8e3c7cdb14c865db67f69c38e
AugustDG <33916325+AugustDG@users.noreply.github.com>
2021-02-01 14:18:22 -0500

    Update Dev.cs

--- Dev/Dev.cs
+++ Dev/Dev.cs
@@ -6,7 +6,7 @@ namespace Scouts.Backend.Dev
     {
         public static string[] SubscriptionTags { get; set; } = { "default" };
         public static string NewsNotificationHubName { get; set; } = "scouts-news";
-        public static string NewsFullAccessConnectionString { get; set; } = "Endpoint=sb://scouts.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=3FhKkS9zfUTmo+Ch5WK8hsznfvxj7bVFnsgTXpFUS+A=";
+        public static string NewsFullAccessConnectionString { get; set; } = "{CONNECTION_STRING}";
     }
 
     public class DeviceInstallation
